2d9b2fe79308661834f7b02c57b1688b502c4037
davidszekeres <112662774+davidszekeres@users.noreply.github.com>
2023-02-09 17:49:08 +0100

    added url language variable

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,5 +1,7 @@
 (function($) {
-
+  
+const urlParams = new URLSearchParams(window.location.search);
+const selectedLanguage = urlParams.get("lang") || "en";
 const languageSelector = document.getElementById("language-selector");
 const title = document.getElementById("title");
 
@@ -8,10 +10,14 @@ const messages = {
   fr: "Citoyenneté",
   jp: "市民権",
 };
+  
+languageSelector.value = selectedLanguage;
+title.innerHTML = messages[selectedLanguage];
 
 languageSelector.addEventListener("change", function(event) {
   const selectedLanguage = event.target.value;
   title.innerHTML = messages[selectedLanguage];
+  window.location = `?lang=${event.target.value}`;
 });
-
+ 
 })(jQuery);
