8591c24c04e88aed56fcedff9d34e26800609289
davidszekeres <112662774+davidszekeres@users.noreply.github.com>
2023-02-11 15:07:43 +0100

    Create email.js

---
+++ assets/js/email.js
@@ -0,0 +1,47 @@
+(function($) {
+  
+const form = document.getElementById("myForm");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+
+  const data = {
+    personalizations: [
+      {
+        to: [
+          {
+            email: "ma@cwa.nevis.kn"
+          }
+        ],
+        subject: `Message from ${name} (${email})`
+      }
+    ],
+    from: {
+      email: "sender@example.com"
+    },
+    content: [
+      {
+        type: "text/plain",
+        value: message
+      }
+    ]
+  };
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://api.sendgrid.com/v3/mail/send");
+  xhr.setRequestHeader("Authorization", "Bearer YOUR_SENDGRID_API_KEY");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(data));
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.responseText);
+    }
+  };
+});
+
+ 
+})(jQuery);
