7a09829af793d8793a30d9b381ec455136bda83e
davidszekeres <112662774+davidszekeres@users.noreply.github.com>
2023-02-12 17:41:03 +0100

    refactor language.js and add translations

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,22 +1,116 @@
 (function($) {
   
+<!-- check URL for language, default: English --> 
 const urlParams = new URLSearchParams(window.location.search);
 const selectedLanguage = urlParams.get("lang") || "en";
+
+<!-- Setup language selector -->
 const languageSelector = document.getElementById("language-selector");
-const title = document.getElementById("title");
-
-const messages = {
-  en: "CITIZENSHIP",
-  fr: "Citoyenneté",
-  jp: "市民権",
-};
-  
 languageSelector.value = selectedLanguage;
-title.innerHTML = messages[selectedLanguage];
 
+<!-- Add translations here, lookup by "id" -->
+  
+			<!-- BANNER -->
+			const bannerTitle = document.getElementById("banner-title");
+			const bannerTitleContent = {
+				en: "CITIZENSHIP",
+				fr: "CITOYENNETÉ",
+				sp: "CIUDADANÍA",
+				jp: "市民権",
+				cn: "公民身份"
+			};
+
+			const bannerBody = document.getElementById("banner-body");
+			const bannerBodyContent = {
+				en: "BY INVESTMENT PROGRAMME",
+				fr: "PAR PROGRAMME D'INVESTISSEMENT",
+				sp: "POR PROGRAMA DE INVERSIÓN",
+				jp: "投資プログラム別",
+				cn: "按投资方案"
+			};
+
+			const bannerButton = document.getElementById("banner-button");
+			const bannerButtonContent = {
+				en: "Get Started",
+				fr: "Commencez",
+				sp: "Comenzar",
+				jp: "はじめに",
+				cn: "开始吧"
+			};
+  
+      <!-- CITIZENSHIP -->
+      const citizenshipTitle = document.getElementById("citizenship-title");
+      const citizenshipTitleContent = {
+        en: "Saint Kitts and Nevis Citizenship",
+        fr: "Citoyenneté de Saint-Kitts-et-Nevis",
+        sp: "Ciudadanía de San Cristóbal y Nieves",
+        jp: "セントクリストファー・ネイビスの市民権について",
+        cn: "圣基茨和尼维斯公民身份"
+      };
+
+      const citizenshipBody = document.getElementById("citizenship-body");
+      const citizenshipBodyContent = {
+        en: "The Saint Kitts and Nevis Citizenship by Investment Program was <b>established in 1984</b>, making it the oldest program of this kind in the world. In close to 40 years of operation, it has distinguished itself by its investment options and due diligence procedures. To this day, Saint Kitts and Nevis remains the world’s most reputable jurisdiction for the conferral of citizenship by investment.",
+        fr: "Le programme de citoyenneté par investissement de Saint-Kitts-et-Nevis a été <b>établi en 1984</b>, ce qui en fait le plus ancien programme de ce type au monde. En près de 40 ans de fonctionnement, il s'est distingué par ses options d'investissement et ses procédures de diligence raisonnable. À ce jour, Saint-Kitts-et-Nevis reste la juridiction la plus réputée au monde pour l'octroi de la citoyenneté par investissement.",
+        sp: "El Programa de Ciudadanía por Inversión de San Cristóbal y Nieves se <b>estableció en 1984</b>, lo que lo convierte en el programa de este tipo más antiguo del mundo. En sus casi 40 años de funcionamiento, se ha distinguido por sus opciones de inversión y sus procedimientos de diligencia debida. A día de hoy, San Cristóbal y Nieves sigue siendo la jurisdicción más reputada del mundo para la concesión de la ciudadanía por inversión.",
+        jp: "セントクリストファー・ネイビスの投資による市民権プログラムは、1984年に設立され、この種のプログラムとしては世界で最も古いものです。40年近い運営期間中、その投資オプションとデューデリジェンス手続きにより、際立った存在となっています。今日に至るまで、セントクリストファー・ネイビスは、投資による市民権授与において世界で最も評判の高い司法機関となっています。",
+        cn: "圣基茨和尼维斯投资入籍计划成立于1984年<b>，是世界上最古老的此类计划。在近40年的运作中，它以其投资选择和尽职调查程序而闻名。时至今日，圣基茨和尼维斯仍然是世界上最有信誉的投资入籍管辖区。"
+      };
+
+      const citizenshipButton = document.getElementById("citizenship-button");
+      const citizenshipButtonContent = {
+        en: "Learn more",
+        fr: "En savoir plus",
+        sp: "Más información",
+        jp: "もっと詳しく",
+        cn: "了解更多"
+      };
+  
+      <!-- JURISDICTION -->
+      const jurisdictionTitle = document.getElementById("jurisdiction-title");
+      const jurisdictionTitleContent = {
+        en: "Jurisdiction",
+        fr: "Compétence",
+        sp: "Jurisdicción",
+        jp: "管轄区域",
+        cn: "管辖权"
+      };
+
+      const jurisdictionBody = document.getElementById("jurisdiction-body");
+      const jurisdictionBodyContent = {
+        en: "Saint Kitts and Nevis, also known as <b>‘Saint Christopher and Nevis’</b> forms part of the group of islands known as the Lesser Antilles, located some 2,000 km to the southeast of the state of Florida. Saint Kitts and Nevis was home to the first British and French colonies in the Caribbean, and thus has also been titled \"The Mother Colony of the West Indies\". The country has beautiful beaches, reefs, wonderful cool climate, enjoyable throughout the year. The country is also well connected by easy and <b>direct flights</b> from Europe, Canada, multiple cities in the US, and most Caribbean airliners.",
+        fr: "Saint-Kitts-et-Nevis, également connu sous le nom de \"Saint-Christophe-et-Nevis\", fait partie du groupe d'îles connu sous le nom de Petites Antilles, situé à environ 2 000 km au sud-est de l'État de Floride. Saint-Kitts-et-Nevis a abrité les premières colonies britanniques et françaises dans les Caraïbes, ce qui lui a valu le titre de \"colonie mère des Antilles\". The country has beautiful beaches, reefs, wonderful cool climate, enjoyable throughout the year. Le pays est également bien desservi par des vols faciles et directs depuis l'Europe, le Canada, plusieurs villes des États-Unis et la plupart des compagnies aériennes des Caraïbes.",
+        sp: "San Cristóbal y Nieves forma parte del grupo de islas conocido como Antillas Menores, situado a unos 2.000 km al sureste del estado de Florida. San Cristóbal y Nieves fue sede de las primeras colonias británicas y francesas en el Caribe, por lo que también se la ha titulado \"La Colonia Madre de las Indias Occidentales\". El país cuenta con hermosas playas, arrecifes y un maravilloso clima fresco, agradable durante todo el año. Además, el país está bien comunicado por vuelos fáciles y directos desde Europa, Canadá, múltiples ciudades de EE UU y la mayoría de las líneas aéreas del Caribe.",
+        jp: "セントクリストファー・ネイビス」としても知られるセントクリストファー・ネイビスは、小アンティル諸島と呼ばれる島々の一部を構成し、フロリダ州の南東約2,000kmに位置しています。セントクリストファー・ネイビスは、カリブ海で最初のイギリスとフランスの植民地があったため、「西インド諸島のマザーコロニー」とも呼ばれています。美しいビーチ、サンゴ礁、素晴らしい冷涼な気候があり、一年を通して楽しむことができる国です。また、ヨーロッパ、カナダ、アメリカの複数の都市、そしてほとんどのカリブ海の航空会社から簡単な直行便が就航しています。",
+        cn: "圣基茨和尼维斯，也被称为 \"圣克里斯托弗和尼维斯\"，是被称为小安的列斯群岛的一部分，位于佛罗里达州东南方向约2000公里处。圣基茨和尼维斯是英国和法国在加勒比地区的第一批殖民地，因此也被称为 \"西印度群岛的母亲殖民地\"。这个国家有美丽的海滩、珊瑚礁、美妙的凉爽气候，一年四季都很愉快。该国的交通也很便利，有来自欧洲、加拿大、美国多个城市和大多数加勒比海客机的便捷直达航班。"
+      };
+
+      const jurisdictionButton = document.getElementById("jurisdiction-button");
+      const jurisdictionButtonContent = {
+        en: "Learn more",
+        fr: "En savoir plus",
+        sp: "Más información",
+        jp: "もっと詳しく",
+        cn: "了解更多"
+      };
+  
+<!-- Add listener to language selector, change text based on selected language --> 
 languageSelector.addEventListener("change", function(event) {
+  
   const selectedLanguage = event.target.value;
-  title.innerHTML = messages[selectedLanguage];
+  
+  bannerTitle.innerHTML = bannerTitleContent[selectedLanguage];
+  bannerBody.innerHTML = bannerBodyContent[selectedLanguage];
+  bannerButton.innerHTML = bannerButtonContent[selectedLanguage];
+  
+  citizenshipTitle.innerHTML = citizenshipTitleContent[selectedLanguage];
+  citizenshipBody.innerHTML = citizenshipBodyContent[selectedLanguage];
+  citizenshipButton.innerHTML = citizenshipButtonContent[selectedLanguage];
+  
+  jurisdictionTitle.innerHTML = jurisdictionTitleContent[selectedLanguage];
+  jurisdictionBody.innerHTML = jurisdictionBodyContent[selectedLanguage];
+  jurisdictionButton.innerHTML = jurisdictionButtonContent[selectedLanguage];
+  
   window.location = `?lang=${event.target.value}`;
 });
  
